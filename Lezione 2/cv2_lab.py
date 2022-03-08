2be2f6fd4d13f3172e57cf197c2b1d535ef3ada0
axel1143 <alexpd1143@gmail.com>
2022-03-08 09:47:53 +0100

    Aggiunta Lezione 2 Laboratoro

---
+++ Lezione 2/cv2_lab.py
@@ -0,0 +1,27 @@
+import cv2
+
+#Legge immaigne con parametro
+img1 = cv2.imread("Img.png", cv2.IMREAD_UNCHANGED)
+img2 = cv2.imread("Img.png", cv2.IMREAD_GRAYSCALE)
+img3 = cv2.imread("Img.png", cv2.IMREAD_COLOR)
+
+#Mostra immagine in finenstra (lo show, elimina l'alpha)
+cv2.imshow("I1", img1)
+cv2.imshow("I2", img2)
+cv2.imshow("I3", img3)
+
+#Aspetta venga premuto un tasto e memorizza
+key = cv2.waitKey(0)
+
+if key == 27:
+    cv2.imwrite("Img_gray.png", img2) #Scrive un immagine, con titolo
+    cv2.destroyAllWindows()
+elif key == ord('s'):
+    cv2.imwrite("Img:unchanged.png",img1)
+    cv2.destroyAllWindows()
+else:
+    cv2.imwrite("Img_color.png", img3)
+    # Chiude finestra selezionata
+    cv2.destroyWindow("I1")
+    cv2.waitKey(5000)
+    cv2.destroyAllWindows()
---
+++ Lezione 2/matplotlib_lab.py
@@ -0,0 +1,27 @@
+import cv2
+from matplotlib import pyplot as plt
+
+img = cv2.imread("Img.png", cv2.IMREAD_GRAYSCALE)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # usato per invertire i modelli dell'immagine
+
+"""
+plt.imshow(img, cmap='gray', interpolation='bicubic')
+plt.xticks([]), plt.yticks([]),# per nascondere i valori x ed y degli assi
+plt.xlabel("Asse x"), plt.ylabel("Asse y") #aggiungo delle label ad asse x e y
+plt.show()
+"""
+
+# visualizziamo due immagini affiancate (griglia 1x2)
+f = plt.figure()
+f.add_subplot(1, 2, 1) # la griglia in cui inseriamo le due immagini (riga, colonna, posizione_corrente)
+plt.imshow(img)
+plt.xticks([]), plt.yticks([])
+f.add_subplot(1, 2, 2) #stessa griglia, posizione 2 e non 1
+plt.imshow(img, cmap="gray") #se non inserisco la cmap, l'immagine in scala di grigio viene sfalsata
+plt.xticks([]), plt.yticks([])
+plt.show()
+
+# cv2 legge immagine secondo il modello (b, g, r) mentre matplotlib legge con (r, g, b), di conseguenza l'immagine
+# importata con cv2, può presentare variazioni di colori se mostrata con matplotlib
+# l'informazione sull'alpha viene mantenuta però in matplotlib
+# se però l'immagine è in GRAY_SCALE e cambio in BGR, non ottengo immagine a colore.
