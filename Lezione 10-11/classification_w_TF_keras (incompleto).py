c3cac78f5fb865781097a5216fe27ab3f98a2269
axel1143 <44553513+axel1143@users.noreply.github.com>
2022-04-18 10:44:52 +0200

    aggiunto file .ipynb per lezione 11

--- Lezione 10-11/classification_w_TF_keras (incompleto).py
+++ Lezione 10-11/classification_w_TF_keras (incompleto).py
@@ -18,6 +18,7 @@ from tensorflow import keras
 from keras import layers
 from keras.models import Sequential
 
+
 # importiamo dataset
 dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
 data_dir = keras.utils.get_file('flower_photos', origin=dataset_url, untar=True)
@@ -49,14 +50,36 @@ train_ds = keras.utils.image_dataset_from_directory(
     batch_size=batch_size
 )
 
-#visualizziamo alcuni dati
+# visualizziamo alcuni dati
 
 plt.figure(figsize=(10, 10))
 for images, labels in train_ds.take(1):
     for i in range(9):
-        ax = plt.subplot(3, 3, i+1)
+        ax = plt.subplot(3, 3, i + 1)
         plt.imshow(images[i].numpy().astype('uint8'))
         plt.title(labels[i])
         plt.axis("off")
 
+for image_batch, labels_batch in train_ds:
+    print(image_batch.shape)
+    print(labels_batch.shape)
+    break
 
+#tuning dataset performance
+# - `Dataset.cache` keeps the images in memory after they're loaded off disk during the first epoch.
+# This will ensure the dataset does not become a bottleneck while training your model.
+# If your dataset is too large to fit into memory, you can also use this method to create a performant on-disk cache.
+# - `Dataset.prefetch` overlaps data preprocessing and model execution while training.
+AUTOTUNE = tf.data.AUTOTUNE
+train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+# standardize the data
+normalization_layer = layers.Rescaling(1./255)  # Range [0,255] -> [0. , 1]
+
+normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+image_batch, labels_batch = next(iter(normalized_ds))
+first_image = image_batch[0]
+# Notice the pixel values are now in `[0,1]`.
+
+print(np.min(first_image), np.max(first_image))
